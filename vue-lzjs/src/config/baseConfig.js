4af49831aa8c9402877f6277bb4cce27cba1dfa1
hutingqing <526958911@qq.com>
2023-06-12 15:07:34 +0800

    gate 图片 补全

--- vue-lzjs/src/config/baseConfig.js
+++ vue-lzjs/src/config/baseConfig.js
@@ -5,6 +5,16 @@ export const imgGate = [
       tooltip: "H",
     },
     {
+      name: "T",
+      url: require("../assets/gate/T.png"),
+      tooltip: "T",
+    },
+    {
+      name: "S",
+      url: require("../assets/gate/S.png"),
+      tooltip: "S",
+    },
+    {
       name: "X",
       url: require("../assets/gate/X.png"),
       tooltip: "X",
@@ -19,6 +29,136 @@ export const imgGate = [
       url: require("../assets/gate/Z.png"),
       tooltip: "Z",
     },
+    {
+      name: "X1",
+      url: require("../assets/gate/X1.png"),
+      tooltip: "X1",
+    },
+    {
+      name: "Y1",
+      url: require("../assets/gate/Y1.png"),
+      tooltip: "Y1",
+    },
+    {
+      name: "Z1",
+      url: require("../assets/gate/Z1.png"),
+      tooltip: "Z1",
+    },
+    {
+      name: "U1",
+      url: require("../assets/gate/U1.png"),
+      tooltip: "U1",
+    },
+    {
+      name: "U2",
+      url: require("../assets/gate/U2.png"),
+      tooltip: "U2",
+    },
+    {
+      name: "U3",
+      url: require("../assets/gate/U3.png"),
+      tooltip: "U3",
+    },
+    {
+      name: "U4",
+      url: require("../assets/gate/U4.png"),
+      tooltip: "U4",
+    },
+    {
+      name: "RX",
+      url: require("../assets/gate/RX.png"),
+      tooltip: "RX",
+    },
+    {
+      name: "RY",
+      url: require("../assets/gate/RY.png"),
+      tooltip: "RY",
+    },
+    {
+      name: "RZ",
+      url: require("../assets/gate/RZ.png"),
+      tooltip: "RZ",
+    },
+    {
+      name: "CNOT",
+      url: require("../assets/gate/CNOT.png"),
+      tooltip: "CNOT",
+    },
+    {
+      name: "ISWAP",
+      url: require("../assets/gate/ISWAP.png"),
+      tooltip: "ISWAP",
+    },
+    {
+      name: "SQISWAP",
+      url: require("../assets/gate/SQISWAP.png"),
+      tooltip: "SQISWAP",
+    },
+    {
+      name: "SWAP",
+      url: require("../assets/gate/SWAP.png"),
+      tooltip: "SWAP",
+    },
+    {
+      name: "TOFFOLI",
+      url: require("../assets/gate/TOFFOLI.png"),
+      tooltip: "TOFFOLI",
+    },
+    {
+      name: "CR",
+      url: require("../assets/gate/CR.png"),
+      tooltip: "CR",
+    },
+    {
+      name: "CZ",
+      url: require("../assets/gate/CZ.png"),
+      tooltip: "CZ",
+    },
+    {
+      name: "MESEAGE",
+      url: require("../assets/gate/MESEAGE.png"),
+      tooltip: "MESEAGE",
+    },
+    {
+      name: "BARRIER",
+      url: require("../assets/gate/BARRIER.png"),
+      tooltip: "BARRIER",
+    },
+    {
+      name: "GHZ2",
+      url: require("../assets/gate/GHZ2.png"),
+      tooltip: "GHZ2",
+    },
+    {
+      name: "GHZ3",
+      url: require("../assets/gate/GHZ3.png"),
+      tooltip: "GHZ3",
+    },
+    {
+      name: "GHZ6",
+      url: require("../assets/gate/GHZ6.png"),
+      tooltip: "GHZ6",
+    },
+    {
+      name: "QFT3",
+      url: require("../assets/gate/QFT3.png"),
+      tooltip: "QFT3",
+    },
+    {
+      name: "QFT4",
+      url: require("../assets/gate/QFT4.png"),
+      tooltip: "QFT4",
+    },
+    {
+      name: "ZCNOT",
+      url: require("../assets/gate/ZCNOT.png"),
+      tooltip: "ZCNOT",
+    },
+    {
+      name: "H6",
+      url: require("../assets/gate/H6.png"),
+      tooltip: "H6",
+    },    
 ]
 
 export const initParam = {
