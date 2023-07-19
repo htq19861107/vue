d037be0383b14eee0ae9679d1e6b9f613c890952
htq19861107 <526958911@qq.com>
2023-07-19 10:49:17 +0800

    完成拖动效果，还存在部分问题

--- vue-lzjs/src/pages/compute/components/ComputePanel.vue
+++ vue-lzjs/src/pages/compute/components/ComputePanel.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="contentPanel">
     <div class="dragicon" tag="ul" key="transition">
-      <span class="quantumGates" v-for="item in imglist" :key="item">
+      <span class="quantumGates" v-for="(item, index) in imglist" :key="index">
         <el-tooltip placement="top">
-          <img
-            :src="item.url"
-            :draggable="true"
-            @dragstart="dragStart(item)"
-            @drop="dragDrop(item,$event)"
-          />
-          <span>鼠标悬浮时显示提示</span>
+          <img :src="item.url" :draggable="item.drag" @dragstart="dragStart(item)" @dragend="dragend(item, $event)" />
           <template v-slot:content>
             <div class="tooltip-content">
               <p>{{ item.tooltip }}</p>
@@ -23,45 +17,33 @@
             <div class="panelTitle">{{ indexCol }}</div>
           </li>
         </ul>
-        <div
-          class="svgrow"
-          v-for="(itemRow, indexRow) in bglist"
-          :key="indexRow"
-        >
-          <span
-            class="qubitNum"
-            @mouseenter="mouseEnter(indexRow)"
-            @mouseleave="mouseLeave(indexRow)"
-            >Q{{ indexRow }}</span
-          >
-          <el-icon @click="clickRemove(indexRow)"><Remove /></el-icon>
-          <span
-            class="svgbg"
-            v-for="(item, indexCol) in itemRow"
-            :key="indexCol"
-            :draggable="true"
-            @dragstart="dragStart(item)"
-            @dragover="dragOver(item,$event)"
-            @drop="dragDrop(item,$event)"
-            @click="judgClick(item)"
-          >
-            <img :src="item.url" />
-            <computeParamSet v-if="paramShow"/>
-            <ShowMsg v-if="show" :message="toastMessage"/>
+        <div class="svgrow" v-for="(itemRow, indexRow) in bglist" :key="indexRow">
+          <span class="qubitNum">Q{{ indexRow }}</span>
+          <el-icon @click.stop="setImgList('reduce', indexRow)">
+            <Remove />
+          </el-icon>
+          <span class="svgbg" v-for="(item, indexCol) in itemRow" :key="indexCol" :ref="handleRef"
+            @dragstart="dragStart(item)" @dragover="dragOver(item, $event)" @dragend="dragend(item, $event)"
+            @drop="dropInner(item)" @click="judgClick(item)" @dragleave="dragLeave(item, $event)">
+            <img :src="item.url" :draggable="item.drag" />
+            <computeParamSet v-if="paramShow" />
+            <ShowMsg v-if="show" :message="toastMessage" />
           </span>
         </div>
-        <el-icon @click="clickAdd()"><CirclePlus /></el-icon>
+        <el-icon @click.stop="setImgList('add')">
+          <CirclePlus />
+        </el-icon>
       </div>
-    </div>  
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, onMounted } from "vue";
+import { reactive, onMounted, ref, nextTick } from "vue";
 import { useStore } from "vuex";
 import { imgGate, initParam } from "../../../config/baseConfig";
 import computeParamSet from "../components/ComputeParamSet"
-import ShowMsg , { useToastEffect } from "../../../components/ShowMsg"
+import ShowMsg, { useToastEffect } from "../../../components/ShowMsg"
 export default {
   name: "computePanel",
   components: {
@@ -69,18 +51,33 @@ export default {
     ShowMsg
   },
   setup() {
-    const { show, toastMessage,showToast } = useToastEffect()
-    const {Qubits,QubitsLineDepth,LineBg,HollowCircle,SolidCircle} = initParam.computePanel;
+    const { show, toastMessage, showToast } = useToastEffect()
+    const { Qubits, QubitsLineDepth, LineBg, HollowCircle, SolidCircle } = initParam.computePanel;
     const store = useStore();
     const imglist = reactive(imgGate);
     let bglist = reactive([]);
     let qubitsArray = [];
-    let startDataUrl = "";
-    let endDataUrl = "";
     let startData = null;
     let endData = null;
     let clickStatus = 0;
     let paramShow = false;
+    let bgRef = ref([]);
+    let doubleRef = ref([]); // dom二维数组
+    let isHaveEle = null;
+    function deepClone(obj) {
+      var target = {};
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+          if (typeof obj[key] === 'object') {
+            target[key] = deepClone(obj[key]);
+          } else {
+            target[key] = obj[key];
+          }
+        }
+      }
+      return target;
+    }
+
     const initBglist = () => {
       for (let row = 0; row < Qubits; row++) {
         let imgRow = [];
@@ -90,7 +87,9 @@ export default {
             col: col,
             drag: false,
             url: LineBg,
-            click:false,
+            click: false,
+            up: 0,//中心点向上偏移
+            down: 0//中心点向下偏移
           });
         }
         if (imgRow.length > 0) {
@@ -98,50 +97,161 @@ export default {
         }
       }
     };
+    /*** 初始化按钮组*/
     const initQubitsArray = () => {
       for (let row = 0; row < Qubits; row++) {
         let Row = [];
         qubitsArray.push(Row);
       }
     };
-    const dragStart = (value) => {
-      startData = value;
-    };
 
-    const dragOver = (value,e) => {
-      endData = value;
+    /*** 按钮组移动*/
+    const dragStart = (value) => {
+      startData = deepClone(value);
+    };
+    const dragOver = (value, e) => {
+      endData = deepClone(value);
+      const { row, col } = value;
+      drawBackgroundChange("access", row, col, startData.up, startData.down);
       e.preventDefault();
     };
+    /**
+    * 离开背景色改为白色
+    */
+    const dragLeave = (value,e) => {
+      const { row, col } = value;
+      drawBackgroundChange("leave", row, col, startData.up, startData.down);
+      e.preventDefault();
+    };
+    /*** 切割为二维数组*/
+    const spliceDoubleArr = (num, arr) => {
+      let newArr = [];
+      const total = Math.ceil(arr.length / 100); // 20
+      for (let i = 0; i < total; i++) {
+        let res = bgRef._rawValue.slice(i * 100, (i + 1) * 100);
+        newArr.push(res);
+      }
+      doubleRef.value = newArr;
+    };
+    const dragend = (item, e) => {
+      let bCanMove = isCanMove(startData, endData)
 
-    const dragDrop = (item,e) => {
-      if (item != null) {
-        startDataUrl = startData.url;
-        endDataUrl = endData.url;
+      if (item != null && bCanMove) {
         const bDrag = startData.drag;
-        if('paramSet' in startData){
+        if ('paramSet' in startData) {
           bglist[endData.row][endData.col].paramSet = startData.paramSet;
         }
-        bglist[endData.row][endData.col].url = startDataUrl;
-        bglist[endData.row][endData.col].drag = true;
+        /*多个图片移动数据*/
         if (bDrag) {
-          bglist[startData.row][startData.col].url = endDataUrl;
-          bglist[startData.row][startData.col].drag = endData.drag;
+          let nUp = 1
+          while (nUp <= startData.up) {
+            bglist[startData.row - nUp][startData.col].url = LineBg;
+            bglist[startData.row - nUp][startData.col].drag = false;
+            nUp = nUp + 1;
+          }
+          let nDown = 1
+          while (nDown <= Math.abs(startData.down) && Number(startData.row) + Number(nDown) < bglist.length) {
+            bglist[Number(startData.row) + Number(nDown)][startData.col].url = LineBg;
+            bglist[Number(startData.row) + Number(nDown)][startData.col].drag = false;
+            nDown = nDown + 1;
+          }
+          bglist[startData.row][startData.col].url = LineBg;
+          bglist[startData.row][startData.col].drag = false;
+          bglist[startData.row][startData.col].up = 0;
+          bglist[startData.row][startData.col].down = 0;
+
+          bglist[endData.row][endData.col].up = startData.up;
+          bglist[endData.row][endData.col].down = startData.down;
+          nUp = 1
+          while (nUp <= startData.up) {
+            bglist[endData.row - nUp][endData.col].url = SolidCircle;
+            bglist[endData.row - nUp][endData.col].drag = false;
+            nUp = nUp + 1;
+          }
+          nDown = 1
+          while (nDown <= Math.abs(startData.down)) {
+            bglist[Number(endData.row) + Number(nDown)][endData.col].url = SolidCircle;
+            bglist[Number(endData.row) + Number(nDown)][endData.col].drag = false;
+            nDown = nDown + 1;
+          }
+
         }
-        if('control' in startData){
-          for(let row =0;row < bglist.length;row++){
-            if(endData.row !== row){
+        if ('control' in startData) {
+          for (let row = 0; row < bglist.length; row++) {
+            if (endData.row !== row) {
               bglist[row][endData.col].url = HollowCircle;
               bglist[row][endData.col].click = true;
+              bglist[row][endData.col].drag = false;
               bglist[row][endData.col].control = startData.control;
             }
           }
           clickStatus = startData.control;
         }
+        bglist[endData.row][endData.col].url = startData.url;
+        bglist[endData.row][endData.col].drag = true;
+        /*清除背景*/
+        drawBackgroundChange("leave", endData.row, endData.col, startData.up, startData.down)
       }
+      drawBackgroundChange("leave", endData.row, endData.col, startData.up, startData.down)
       e.preventDefault();
     };
-    const mouseEnter = () => {};
-    const mouseLeave = () => {};
+
+    /**
+    * 判断整体是否可挪动
+    * @returns true & false
+    */
+    /*** 背景阴影* @param { String } type access:进入 & leave:离开*/
+    const drawBackgroundChange = (type, row, col, up, down) => {
+      const colorMap = {
+        access: "gray", // 鼠标移入背景
+        leave: "white", // 鼠标离开背景
+      };
+      if (up > 0) {
+        let val = 1;
+        while ((val <= up) && ((row - val) >= 0)) {
+          doubleRef._rawValue[row - val][col].style.background = colorMap[type];
+          val++;
+        }
+      }
+      if (down < 0) {
+        let val = 1;
+        while ((val <= Math.abs(down)) && ((Number(row) + Number(val)) < bglist.length)) {
+          doubleRef._rawValue[Number(row) + Number(val)][col].style.background = colorMap[type];
+          val++;
+        }
+      }
+
+      if (typeof (row) != "undefined" && typeof (col) != "undefined") {
+        doubleRef._rawValue[row][col].style.background = colorMap[type];
+      }
+
+
+    };
+    /*** 是否可移动*/
+    const dropInner = (val) => {
+      // 超出边界
+      isHaveEle = true;
+      if (startData.row - startData.up <= val.row && Number(startData.row) + Number(Math.abs(startData.down)) > bglist.length) {
+        isHaveEle = false;
+      }
+    };
+    const isCanMove = (src, des) => {
+      // 超出边界
+      isHaveEle = true;
+      if (src.hasOwnProperty('row')) {
+        if (des.row - src.up < 0 || Number(des.row) + Number(Math.abs(src.down)) >= bglist.length) {
+
+          isHaveEle = false;
+        }
+      }
+      return isHaveEle;
+    };
+    const handleRef = (el) => {
+      if (el) {
+        bgRef.value.push(el);
+      }
+    };
+
     const clickRemove = (index) => {
       if (index > -1) {
         bglist.splice(index, 1);
@@ -157,7 +267,7 @@ export default {
           col: iCol,
           url: LineBg,
           drag: false,
-          click:false,
+          click: false,
         };
         bgCol.push(objBg);
       }
@@ -165,68 +275,102 @@ export default {
       let rowData = [];
       store.commit("ADDqubitsArray", { rowData });
     };
-    const showFloatParamSet = ()=>{
+    const showFloatParamSet = () => {
       paramShow = !paramShow;
     };
-    const showAlert = (item)=>{
-      //alert("message")
-      return item.click;
-    };
-    const judgClick = (item)=>{
-      console.log(item);
-      console.log(clickStatus)
-      
-      if(0 === clickStatus){
+    const judgClick = (item) => {
+      if (0 === clickStatus) {
         showFloatParamSet();
       }
-      else{
-        if('control' in item){
-          SetControlGate(item);      
+      else {
+        if ('control' in item) {
+          SetControlGate(item);
         }
-        else{
+        else {
           showToast("请点击垂直方向上高亮的点");
-        }        
+        }
       }
     };
-    const SetControlGate = (value) => { 
+    const SetControlGate = (value) => {
       const nContr = value.control;
-      console.log(value.control)
-      if('click' in value && value.click){
+      if ('click' in value && value.click) {
         value.url = SolidCircle
-        for(let row = 0;row < bglist.length;row++){
-          if('control' in bglist[row][value.col]){
+        for (let row = 0; row < bglist.length; row++) {
+          if ('control' in bglist[row][value.col]) {
             bglist[row][value.col].control = nContr - 1;
           }
         }
         clickStatus = clickStatus - 1;
         value.click = false;
+        bglist[endData.row][endData.col].up = (endData.row - value.row) > bglist[endData.row][endData.col].up ? endData.row - value.row : bglist[endData.row][endData.col].up;
+        bglist[endData.row][endData.col].down = (value.row - endData.row) > bglist[endData.row][endData.col].down ? endData.row - value.row : bglist[endData.row][endData.col].down;
+
       }
-      if(value.control === 0){
-        for(let row = 0;row < bglist.length;row++){
-          if(value.row !== row && bglist[row][value.col].url === HollowCircle){
+      if (value.control === 0) {
+        for (let row = 0; row < bglist.length; row++) {
+          if (value.row !== row && bglist[row][value.col].url === HollowCircle) {
             bglist[row][value.col].url = LineBg;
             bglist[row][value.col].click = false;
           }
         }
       }
-    }
+    };
+    /*** 删除跟增加* @param { String } type add 增加 reduce 减少*/
+    const setImgList = (type, index = null) => {
+      const imgMap = new Map([
+        [
+          "add",
+          () => {
+            let newImgArr = [];
+            for (let i = 0; i < QubitsLineDepth; i++) {
+              newImgArr.push({
+                row: bglist.length,
+                col: i,
+                drag: false, // 是否为已选择的指示点
+                url: LineBg,
+              });
+            }
+            bgRef._rawValue = [];
+            bglist.push(newImgArr);
+            nextTick(() => {
+              spliceDoubleArr(100, bgRef._rawValue);
+            });
+          },
+        ],
+        [
+          "reduce",
+          () => {
+            if (bglist.length <= 1) {
+              return;
+            }
+            bglist.splice(index, 1);
+          },
+        ],
+      ]);
+      imgMap.get(type)();
+    };
     onMounted(() => {
       initBglist();
       initQubitsArray();
       store.commit("INITqubitsArray", { qubitsArray });
+      // 处理表格dom
+      nextTick(() => {
+        spliceDoubleArr(100, bgRef._rawValue);
+      });
     });
     return {
       clickStatus,
       dragStart,
       dragOver,
-      dragDrop,
-      mouseEnter,
-      mouseLeave,
+      dragend,
+      dropInner,
+      dragLeave,
       clickRemove,
       clickAdd,
       SetControlGate,
       judgClick,
-      showAlert,
+      setImgList,
+      handleRef,
       Qubits,
       QubitsLineDepth,
       qubitsArray,
@@ -246,8 +390,9 @@ export default {
   left: 0px;
   top: 150px;
   width: 100%;
-  padding-left:15px;
+  padding-left: 15px;
   box-sizing: border-box;
+
   .dragicon {
     .quantumGates {
       ul {
@@ -256,6 +401,7 @@ export default {
         top: 0px;
         list-style-type: none;
         display: block;
+
         li {
           position: relative;
           display: inline-block;
@@ -264,12 +410,14 @@ export default {
         }
       }
     }
+
     .computebg {
       width: 1100px;
       height: 400px;
       overflow-x: scroll;
       overflow-y: scroll;
-      margin-top:20px;
+      margin-top: 20px;
+
       .panelTitle {
         position: relative;
         left: 0px;
@@ -277,6 +425,7 @@ export default {
         padding: 10px;
         white-space: nowrap;
       }
+
       ul {
         position: relative;
         left: 0px;
@@ -287,7 +436,7 @@ export default {
         padding-left: 36px;
 
         li {
-          width:32px;
+          width: 32px;
           height: 32px;
           text-align: center;
           line-height: 32px;
@@ -295,10 +444,12 @@ export default {
           display: inline-block;
         }
       }
+
       .svgrow {
         display: flex;
         align-items: center;
         white-space: nowrap;
+
         .qubitNum {
           text-align: center;
           right: 0;
@@ -310,9 +461,11 @@ export default {
           display: inline-block;
           width: 20;
         }
+
         .svgbg {
           width: 32px;
           height: 32px;
+
           img {
             margin: 0 0px 0 0;
           }
